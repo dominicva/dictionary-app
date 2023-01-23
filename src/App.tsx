64bf3c7304b13c7b750563458e0f36ce2c5dd497
Dom van Almsick <dominicvana@gmail.com>
2023-01-22 21:01:08 -0500

    feat: init styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Meaning from "./components/Meaning";
 import logo from "./assets/images/logo.svg";
+import moonIcon from "./assets/images/icon-moon.svg";
 import searchIcon from "./assets/images/icon-search.svg";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   const [audioUrl, setAudioUrl] = useState("");
   const [source, setSource] = useState("");
   const [meanings, setMeanings] = useState([]);
+  const [theme, setTheme] = useState("light");
 
   async function handleSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
@@ -40,7 +42,7 @@ function App() {
   }
 
   return (
-    <div className="p-4">
+    <div className="p-6">
       <header className="flex items-center justify-between">
         <div>
           <img src={logo} alt="Logo" />
@@ -50,12 +52,23 @@ function App() {
           <option value="">Serif</option>
           <option value="">Mono</option>
         </select>
-        <input type="checkbox" className="block" />
+
+        <label className="relative inline-flex cursor-pointer items-center">
+          <input type="checkbox" value="" className="peer sr-only" />
+          <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            <img src={moonIcon} alt="Color mode" />
+          </span>
+        </label>
       </header>
-      <form className="mt-6 mb-7 flex justify-center" onSubmit={handleSubmit}>
-        <div>
-          <input type="text" placeholder="Search for any word" />
-          <button>
+      <form className="mt-6 mb-7 flex" onSubmit={handleSubmit}>
+        <div className="relative w-80">
+          <input
+            type="text"
+            placeholder="Search for any word"
+            className="h-12 w-full rounded-2xl border-none bg-gray-200"
+          />
+          <button className="absolute right-4 top-1/3">
             <img src={searchIcon} alt="Search" />
           </button>
         </div>
@@ -63,8 +76,8 @@ function App() {
       <section>
         <div className="mb-8 flex justify-between">
           <div>
-            <h1 className="mb-2 text-2xl font-bold">{word}</h1>
-            <p>/{phonetic}</p>
+            <h1 className="mb-2 text-3xl font-bold">{word}</h1>
+            <p className="text-lg text-purple-500">/{phonetic}</p>
           </div>
           <button>
             <a href={audioUrl} target="_blank">
--- src/components/Meaning.tsx
+++ src/components/Meaning.tsx
@@ -1,13 +1,19 @@
 export default function Meaning({ partOfSpeech, definitions, synonyms }) {
   return (
-    <div>
+    <div className="mb-8">
       <h2 className="mb-8 font-bold italic">{partOfSpeech}</h2>
       <h3 className="mb-4">Meaning</h3>
-      <ul>
+      <ul className="ml-8 list-inside list-disc">
         {definitions.map(({ definition, example }) => (
-          <li key={`${definition}${Math.random()}`}>
-            {definition}
-            {example ? <q>{example}</q> : null}
+          <li
+            key={`${definition}${Math.random()}`}
+            className="mb-3 marker:text-purple-700"
+            style={{ textIndent: "-2rem" }}
+          >
+            <span className="ml-4">
+              {definition}
+              {example ? <q>{example}</q> : null}
+            </span>
           </li>
         ))}
       </ul>
