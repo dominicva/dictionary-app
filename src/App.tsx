1bf0c119fd4079f3b51ab2cde15e80e967a59504
Dom van Almsick <dominicvana@gmail.com>
2023-01-22 19:32:30 -0500

    feat: rendering API results

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,42 @@
+import React, { useState } from "react";
 import searchIcon from "./assets/images/icon-search.svg";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [phonetic, setPhonetic] = useState("");
+  const [audioUrl, setAudioUrl] = useState("");
+  const [source, setSource] = useState("");
+  const [meanings, setMeanings] = useState([]);
+
+  async function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+
+    try {
+      const word = e.target[0].value;
+      const res = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      ).then(r => r.json());
+      const { meanings, phonetics, sourceUrls } = res[0];
+      const { text, audio } = phonetics.find(
+        o =>
+          Object.hasOwn(o, "audio") &&
+          Object.hasOwn(o, "text") &&
+          o.audio.length > 0 &&
+          o.text.length > 0
+      );
+
+      setMeanings(meanings);
+      setWord(word);
+      setPhonetic(text);
+      setAudioUrl(audio);
+      setSource(sourceUrls[0] ?? "no source URL found");
+    } catch (error) {
+      console.error(
+        `ERROR: could not find definition for ${word}\n\n\t${error}`
+      );
+    }
+  }
+
   return (
     <div className="p-4">
       <header className="flex items-center justify-between">
@@ -12,7 +48,7 @@ function App() {
         </select>
         <input type="checkbox" className="block" />
       </header>
-      <form className="mt-6 mb-7 flex justify-center">
+      <form className="mt-6 mb-7 flex justify-center" onSubmit={handleSubmit}>
         <div>
           <input type="text" placeholder="Search for any word" />
           <button>
@@ -23,26 +59,57 @@ function App() {
       <section>
         <div className="mb-8 flex justify-between">
           <div>
-            <h1>keyboard</h1>
-            <p>/'ki:add</p>
+            <h1 className="mb-2 text-2xl font-bold">{word}</h1>
+            <p>/{phonetic}</p>
           </div>
-          <button>Play</button>
+          <button>
+            <a href={audioUrl} target="_blank">
+              Play
+            </a>
+          </button>
         </div>
-        <h2>Noun</h2>
-        <h3>Meaning</h3>
-        <ul>
-          <li>defino</li>
-          <li>lorem</li>
-          <li>ipsum</li>
-        </ul>
-        <hr />
+
+        {meanings.map(({ partOfSpeech, definitions, synonyms }) => (
+          <Meaning
+            partOfSpeech={partOfSpeech}
+            definitions={definitions}
+            synonyms={synonyms}
+          />
+        ))}
+
+        <hr className="mt-8 mb-6" />
         <div>
           <h4>Source</h4>
-          <a href="https://en.wiktionary.org/wiki/keyboard" target="_blank">
-            https://en.wiktionary.org/wiki/keyboard
+          <a href={source} target="_blank">
+            {source}
           </a>
         </div>
       </section>
+    </div>
+  );
+}
+
+function Meaning({ partOfSpeech, definitions, synonyms }) {
+  return (
+    <div>
+      <h2 className="mb-8 font-bold italic">{partOfSpeech}</h2>
+      <h3 className="mb-4">Meaning</h3>
+      <ul>
+        {definitions.map(({ definition, example }) => (
+          <>
+            <li key={`${definition}${Math.random()}`}>{definition}</li>
+            {example ? <q>{example}</q> : null}
+          </>
+        ))}
+      </ul>
+      <div>
+        {partOfSpeech === "noun" ? (
+          <>
+            <h4>Synonyms</h4>
+            <p>{synonyms[0]}</p>
+          </>
+        ) : null}
+      </div>
     </div>
   );
 }
