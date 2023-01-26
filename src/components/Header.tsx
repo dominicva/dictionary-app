02fb39cce251926550972a62e00e5f3e7aee0d43
Dom van Almsick <dominicvana@gmail.com>
2023-01-25 19:15:45 -0500

    feat: minor header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,29 +13,37 @@ export default function Header({ setTheme }) {
   }
 
   return (
-    <header className="flex items-center justify-between bg-white dark:bg-black">
+    <header className="flex items-center justify-between">
       <div>
         <img src={logo} alt="Logo" />
       </div>
-      <select name="font" id="font">
-        <option value="">Sans Serif</option>
-        <option value="">Serif</option>
-        <option value="">Mono</option>
-      </select>
+      <div className="flex items-center">
+        <select
+          name="font"
+          id="font"
+          className="w-30 border-none text-lg font-bold leading-8"
+        >
+          <option value="">Sans Serif</option>
+          <option value="">Serif</option>
+          <option value="">Mono</option>
+        </select>
 
-      <label className="relative inline-flex cursor-pointer items-center">
-        <input
-          type="checkbox"
-          value={theme}
-          className="peer sr-only"
-          checked={theme === "dark"}
-          onChange={switchTheme}
-        />
-        <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
-        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-          <img src={moonIcon} alt="Color mode" />
-        </span>
-      </label>
+        <hr className="h-0.25 w-8 rotate-90 text-gray-light" />
+
+        <label className="relative inline-flex cursor-pointer items-center">
+          <input
+            type="checkbox"
+            value={theme}
+            className="peer sr-only"
+            checked={theme === "dark"}
+            onChange={switchTheme}
+          />
+          <div className="bg-gray-200 after:border-gray-300 after:bg-white peer-checked:bg-blue-600 peer-checked:after:border-white peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800 peer h-6 w-11 rounded-full after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-focus:outline-none peer-focus:ring-4"></div>
+          <span className="text-gray-900 dark:text-gray-300 ml-3 text-sm font-medium">
+            <img src={moonIcon} alt="Color mode" />
+          </span>
+        </label>
+      </div>
     </header>
   );
 }
